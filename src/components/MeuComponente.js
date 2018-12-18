a624e197bfb71c05bbf36ddd53815f194aff00c6
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-18 10:05:48 -0200

    aula-02-02

--- src/components/MeuComponente.js
+++ src/components/MeuComponente.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+/*
 function MeuComponente(){
     return <h1>TreinaWeb</h1>;
 }
+*/
 
 /*
 const MeuComponente = () => {
     return  <h1>TreinaWeb</h1>;
 }
 */
+
+class MeuComponente extends Component{
+    render(){
+        return <h1>TreinaWeb</h1>;
+    }
+}
 
 export default MeuComponente;
