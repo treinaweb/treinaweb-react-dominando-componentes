0ef944f424c12c2a6308dcc8da76c2e408e00fda
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-20 08:31:13 -0200

    aula-05-04

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 
 const myList = [
-    {id: 123,nome: "João", idade: 23},
-    {id: 456,nome: "Maria", idade: 25},
-    {id: 789,nome: "Paulo", idade: 32},
-    {id: 321,nome: "Bruna", idade: 20}
+    {id: 123, nome: "João", idade: 23},
+    {id: 456, nome: "Maria", idade: 25},
+    {id: 789, nome: "Paulo", idade: 32},
+    {id: 321, nome: "Bruna", idade: 20}
 ]
 
 
 class PeopleList extends Component{
+
+    sayMyName(person){
+        alert(person.nome);
+    }
+
     render(){
         return (
             <ul>
-                { myList.map(person => <li key={person.id} >{person.nome} - {person.idade} anos</li>) }
+                { myList.map(person => 
+                    <li onClick={this.sayMyName.bind(this, person)} key={person.id} >{person.nome} - {person.idade} anos</li>
+                  ) 
+                }
             </ul>
         );
     }
