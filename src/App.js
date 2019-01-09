d1af2311fb128e8ea78c614c20fdc4240ad062ec
Akira Hanashiro <felipehanashiro@hotmail.com>
2019-01-09 15:49:51 -0200

    projeto-07-01

--- src/App.js
+++ src/App.js
@@ -15,13 +15,15 @@ class App extends Component {
     super(props);
 
     this.selectVideo = this.selectVideo.bind(this);
+    this.toggleCinema = this.toggleCinema.bind(this);
 
     this.inlineVideo = React.createRef();
     this.cinemaVideo = React.createRef();
 
     this.state = {
       videos: [],
-      selectedVideo: {}
+      selectedVideo: {},
+      videoContainerElement: this.inlineVideo
     }
   }
 
@@ -29,10 +31,20 @@ class App extends Component {
     const videos = await VideoService.list();
     this.setState({videos});
     Channel.on('video:select', this.selectVideo)
+    Channel.on('video:toggle-cinema', this.toggleCinema)
   }
 
   componentWillUnmount(){
     Channel.removeListener('video:select', this.selectVideo);
+    Channel.removeListener('video:toggle-cinema', this.toggleCinema);
+  }
+
+  toggleCinema(){
+    const currentElement = this.state.videoContainerElement,
+      newContainer = currentElement === this.inlineVideo ? this.cinemaVideo : this.inlineVideo;
+    this.setState({
+      videoContainerElement: newContainer
+    })
   }
 
   selectVideo(video){
@@ -43,12 +55,12 @@ class App extends Component {
     const { state } = this;
     return (
       <div className="App">
-       <VideoPlayer video={state.selectedVideo} />
+       <VideoPlayer video={state.selectedVideo} container={state.videoContainerElement.current} />
        <VideoInline>
         <div ref={this.inlineVideo} ></div>
        </VideoInline>
        <VideoList videos={state.videos} />
-       <VideoCinema isActive={false}>
+       <VideoCinema isActive={state.videoContainerElement === this.cinemaVideo}>
         <div ref={this.cinemaVideo} ></div>
        </VideoCinema>
       </div>
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { Channel } from '../services/EventService';
 
 class VideoPlayer extends Component{
 
@@ -6,20 +8,54 @@ class VideoPlayer extends Component{
         super(props);
 
         this.videoElement = React.createRef();
+        this.currentTime = 0;
+
+        this.toggleCinema = this.toggleCinema.bind(this);
+        this.onPlay = this.onPlay.bind(this);
+        this.onStop = this.onStop.bind(this);
+
+    }
+
+    toggleCinema(){
+        this.onStop();
+        Channel.emit('video:toggle-cinema');
+    }
+
+    onPlay(){
+        this.videoElement.current.currentTime = this.currentTime;
+    }
+
+    onStop(){
+        this.currentTime = this.videoElement.current.currentTime;
+    }
+
+    componentDidUpdate(prevProps){
+        if(this.props.video.url !== prevProps.video.url){
+            this.currentTime = 0;
+        }
     }
 
     render(){
-        const { video } = this.props;
-        return (
-            <div className="video-player" >
-                <video
-                    src={video.url}
-                    controls autoPlay loop
-                    ref={this.videoElement}
-                 />
-                 <button>[ ]</button>
-            </div>
-        );
+        const { container, video } = this.props;
+        if(!video.url){
+            return '';
+        }else if(!container){
+            return 'carregando';
+        }else{
+            const element = (
+                <div className="video-player" >
+                    <video
+                        src={video.url}
+                        onPlay={this.onPlay}
+                        onPause={this.onStop}
+                        controls autoPlay loop
+                        ref={this.videoElement}
+                     />
+                     <button onClick={this.toggleCinema} >[ ]</button>
+                </div>
+            );
+            return ReactDOM.createPortal(element, container);
+        }
     }
 }
 
